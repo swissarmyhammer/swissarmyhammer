22b90328ca5dfdca1232b272d16c50b07a9d2a29
Will Ballard <wballard@mailframe.net>
2026-03-29 10:21:45 -0500

    feat: add menu field to YAML CommandDef

--- swissarmyhammer-commands/src/lib.rs
+++ swissarmyhammer-commands/src/lib.rs
@@ -21,5 +21,5 @@ pub use command::Command;
 pub use context::{parse_moniker, CommandContext};
 pub use error::{CommandError, Result};
 pub use registry::{builtin_yaml_sources, load_yaml_dir, CommandsRegistry};
-pub use types::{CommandDef, CommandInvocation, KeysDef, ParamDef, ParamSource};
+pub use types::{CommandDef, CommandInvocation, KeysDef, MenuPlacement, ParamDef, ParamSource};
 pub use ui_state::{DragSession, RecentBoard, UIState, UIStateChange, WindowState};
